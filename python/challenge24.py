c5c0cf676caadb9af06960a4b861c7f620bcda7c
Valmir <vmcfilho@gmail.com>
2023-11-07 15:13:59 -0300

    Add files via upload

---
+++ python/challenge24.py
@@ -0,0 +1,14 @@
+"""
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+"""
+
+
+def solution(string):
+    return string[::-1]
+
+
+result = solution("codewars")
+print(f"The string reversed is {result}.")
